a6e35395af3a1513f370e669c27cdb99bf99d928
ibm_admin <ibm_admin@ADMINIB-U0RGV7D.in.ibm.com>
2017-05-11 16:23:27 +0530

    Signed-off-by: ibm_admin <ibm_admin@ADMINIB-U0RGV7D.in.ibm.com>

---
+++ src/test/java/webDriverQuestion/Question_5_CrossBrowserTesting.java
@@ -0,0 +1,12 @@
+package webDriverQuestion;
+
+import org.testng.annotations.Test;
+
+public class Question_5_CrossBrowserTesting 
+{
+	@Test
+	public void printTitle()
+	{
+		System.out.println("Title of the page : ");
+	}
+}
